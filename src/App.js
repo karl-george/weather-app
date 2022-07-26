ca7a5df1ff5329a9587885024f3a741bf537661a
Karl George <karlgdev@gmail.com>
2022-07-26 14:16:11 +0200

    Working on API call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Forecast from './Components/Forecast';
 import './App.css';
 
 function App() {
@@ -6,22 +7,30 @@ function App() {
   const [long, setLong] = useState();
   const [data, setData] = useState();
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(() => {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`;
 
-    fetch(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setLat(pos.coords.latitude);
+        setLong(pos.coords.longitude);
+      });
+
+      await fetch(url)
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    };
+
+    fetchData();
+  }, [lat, long]);
 
   console.log(data);
 
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      {typeof data !== 'undefined' ? <Forecast /> : <div>Loading...</div>}
+    </div>
+  );
 }
 
 export default App;
