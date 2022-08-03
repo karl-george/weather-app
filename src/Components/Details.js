925097511926f9f42091300d87a296c970a65180
Karl George <karlgdev@gmail.com>
2022-08-03 13:43:05 +0200

    Working on daily details

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,16 +1,32 @@
 import moment from 'moment';
 
 const Details = ({ weatherData }) => {
-  // console.log(weatherData);
+  console.log(weatherData);
 
   return (
-    <div>
-      <p>
-        {moment
-          .unix(weatherData.current.sunrise)
-          .format('h:mm a')
-          .toUpperCase()}
-      </p>
+    <div className='container'>
+      <div className='grid grid--1x2'>
+        <div className='detail-block sunrise-block'>
+          <img src='https://openweathermap.org/img/wn/01d@2x.png'></img>
+          <h4>Sunrise</h4>
+          <p>
+            {moment
+              .unix(weatherData.current.sunrise)
+              .format('h:mm a')
+              .toUpperCase()}
+          </p>
+        </div>
+        <div className='detail-block sunset-block'>
+          <img src='https://openweathermap.org/img/wn/01n@2x.png'></img>
+          <h4>Sunset</h4>
+          <p>
+            {moment
+              .unix(weatherData.current.sunset)
+              .format('h:mm a')
+              .toUpperCase()}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
